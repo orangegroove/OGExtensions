8db7973947e70ed084e73423241cc5ddf0b98a77
Jesper <jesper@orangegroove.net>
2014-01-02 15:52:43 +0200

    updated syntax

--- OGExtensions/NSIndexPath+OGExtensions.h
+++ OGExtensions/NSIndexPath+OGExtensions.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Orange Groove. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface NSIndexPath (OGExtensions)
 
--- OGExtensions/OGTextField.h
+++ OGExtensions/OGTextField.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Orange Groove. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface OGTextField : UITextField
 
